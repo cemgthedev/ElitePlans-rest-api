d5d42c9eea595482e51a15b0287618c0976c8558
cemgthedev <cemg.the.dev@gmail.com>
2025-02-01 16:13:03 -0300

    feat: implementação da rota de remoção de treino do plano

--- src/services/plan_workouts.py
+++ src/services/plan_workouts.py
@@ -7,11 +7,11 @@ from services.configs import plan_workouts_logger
 # Criar roteador
 router = APIRouter()
 
-# Rota de criação de um novo plano de treino
+# Rota de criação de um novo treino do plano
 @router.post('/plan_workouts')
 async def create_plan_workout(plan_workout: PlanWorkouts):
     try:
-        plan_workouts_logger.info(f'Criando plano de treino: {plan_workout}')
+        plan_workouts_logger.info(f'Criando treino do plano: {plan_workout}')
         
         plan = await db.plans.find_one({"_id": ObjectId(plan_workout.plan_id)})
         if not plan:
@@ -28,13 +28,31 @@ async def create_plan_workout(plan_workout: PlanWorkouts):
 
         created_plan_workout = await db.plan_workouts.find_one({"_id": response.inserted_id})
         if not created_plan_workout:
-            plan_workouts_logger.error(f'Erro ao criar plano de treino: {plan_workout}')
-            raise HTTPException(status_code=500, detail='Erro ao criar plano de treino')
+            plan_workouts_logger.error(f'Erro ao criar treino do plano: {plan_workout}')
+            raise HTTPException(status_code=500, detail='Erro ao criar treino do plano')
 
         created_plan_workout["_id"] = str(created_plan_workout["_id"])
-        plan_workouts_logger.info(f'Plano de treino criado com sucesso: {created_plan_workout}')
+        plan_workouts_logger.info(f'Treino do plano criado com sucesso: {created_plan_workout}')
         return created_plan_workout
 
     except Exception as e:
-        plan_workouts_logger.error(f'Erro ao criar plano de treino: {e}')
-        raise HTTPException(status_code=500, detail='Erro ao criar plano de treino')
+        plan_workouts_logger.error(f'Erro ao criar treino do plano: {e}')
+        raise HTTPException(status_code=500, detail='Erro ao criar treino do plano')
+    
+# Rota de remoção de treino de plano
+@router.delete('/plan_workouts/{id}')
+async def delete_plan_workout(id: str):
+    try:
+        plan_workouts_logger.info(f'Excluindo treino do plano: {id}')
+        response = await db.plan_workouts.delete_one({"_id": ObjectId(id)})
+
+        if response.deleted_count == 0:
+            plan_workouts_logger.warning(f'Treino do plano não encontrado: {id}')
+            raise HTTPException(status_code=404, detail='Treino do plano não encontrado')
+
+        plan_workouts_logger.info(f'Treino do plano excluído com sucesso: {id}')
+        return {"message": "Treino do plano excluído com sucesso"}
+
+    except Exception as e:
+        plan_workouts_logger.error(f'Erro ao excluir treino do plano: {e}')
+        raise HTTPException(status_code=500, detail='Erro ao excluir treino do plano')
