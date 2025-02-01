da264f06d5fa7844c50e85abefb766248092b975
cemgthedev <cemg.the.dev@gmail.com>
2025-02-01 10:38:18 -0300

    feat: implementação da rota de atualização de plano

--- src/services/plans.py
+++ src/services/plans.py
@@ -15,6 +15,7 @@ async def create_plan(plan: Plan):
         
         seller = await db.users.find_one({"_id": ObjectId(plan.seller_id)})
         if not seller:
+            plans_logger.warning(f'Vendedor não encontrado: {plan.seller_id}')
             raise HTTPException(status_code=404, detail='Vendedor não encontrado')
         
         plan_dict = plan.dict(by_alias=True, exclude={"id"})
@@ -31,3 +32,29 @@ async def create_plan(plan: Plan):
     except Exception as e:
         plans_logger.error(f'Erro ao criar plano: {e}')
         raise HTTPException(status_code=500, detail='Erro ao criar plano')
+    
+# Rota de atualização de um plano
+@router.put('/plans/{plan_id}')
+async def update_plan(plan_id: str, plan: Plan):
+    try:
+        plans_logger.info(f'Atualizando plano: {plan}')
+        
+        seller = await db.users.find_one({"_id": ObjectId(plan.seller_id)})
+        if not seller:
+            plans_logger.warning(f'Vendedor não encontrado: {plan.seller_id}')
+            raise HTTPException(status_code=404, detail='Vendedor não encontrado')
+        
+        plan_dict = plan.dict(by_alias=True, exclude={"id"})
+        updated_plan = await db.plans.update_one({"_id": ObjectId(plan_id)}, {"$set": plan_dict})
+        
+        if updated_plan.matched_count == 0:
+            raise HTTPException(status_code=404, detail='Plano não encontrado')
+        
+        updated_plan = await db.plans.find_one({"_id": ObjectId(plan_id)})
+        updated_plan["_id"] = str(updated_plan["_id"])
+        plans_logger.info(f'Plano atualizado com sucesso: {updated_plan}')
+        return updated_plan
+    
+    except Exception as e:
+        plans_logger.error(f'Erro ao atualizar plano: {e}')
+        raise HTTPException(status_code=500, detail='Erro ao atualizar plano')
